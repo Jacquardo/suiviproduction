d510bb21500e0a774adc2a2d7970fb03ec9d465e
Jacquardo <sedrajacquardo@gmail.com>
2026-06-24 11:00:07 +0300

    Update cache name to suivi-production-arc-v4

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    Suivi Production ARC+
 ========================= */
 
-const CACHE_NAME = "suivi-production-arc-v3";
+const CACHE_NAME = "suivi-production-arc-v4";
 
 const FILES_TO_CACHE = [
   "./",
